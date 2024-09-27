5e3d676307a6f119bbeacca9f44e8e1d64853fda
schneemann-sun <sun.stgt@gmail.com>
2024-09-27 22:32:48 +0200

    cicd workflows

---
+++ setup/terraform/init.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+export PATH="$HOME/.tfenv/bin:$PATH"
+source ~/.bashrc
+tfenv install 1.3.9
+tfenv use 1.3.9
+cd /workspace/setup/terraform
+terraform init
+terraform plan
+terraform apply
